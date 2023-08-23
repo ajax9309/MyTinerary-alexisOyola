ee61bb97c61324516eb228806b97608260e47f39
ajax9309 <alexzomboe@gmail.com>
2023-08-23 01:57:47 -0300

    sprint2

---
+++ src/components/cardcities/Cardcities.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Cardcities = ({data}) => {
+    const {city,
+        country,
+        language,
+        currency,
+        image,
+        population,
+        description}=data
+  return (
+    <div>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{city}</Card.Title>
+          <Card.Text>
+            {description}
+            {country}
+            {language}
+            {population}
+            {currency}
+          </Card.Text>
+          <Button variant="primary">Discover</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default Cardcities;
--- src/pages/Cities/Cities.jsx
+++ src/pages/Cities/Cities.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
 import MainLayout from '../../layouts/MainLayout'
+import axios from 'axios';
+import Cardcities from '../../components/cardcities/cardcities';
 
 const Cities = () => {
+  const params=useParams()
+  // console.log(params);
+  const [cities, setCities]=useState([])
+  useEffect(()=>{
+    axios('http://localhost:3000/api/cities')
+    .then(res=>setCities(res.data.response))
+    
+
+  },[])
+
   return (
     <MainLayout>
-    <img className='img-fluid rounded mx-auto d-block m-3' src="https://previews.123rf.com/images/jakarin2521/jakarin25211804/jakarin2521180400005/98543408-concept-of-under-construction-website-graphic-of-construction-site-with-industrial-crane.jpg" alt="undercontruction" />
+    
+    <div className='img-fluid rounded mx-auto d-block m-3' src="https://previews.123rf.com/images/jakarin2521/jakarin25211804/jakarin2521180400005/98543408-concept-of-under-construction-website-graphic-of-construction-site-with-industrial-crane.jpg" alt="undercontruction">
+      {/* {
+        cities.map(city=> <Cardcities keys={city._id} data={city} />)
+      }
+      <Cardcities/> */}
+    </div>
     </MainLayout>
   )
 }
